c3f48a51bc6ae2825a0725b03759605cfd3045e6
alex <alextamayop@gmail.com>
2018-03-25 18:12:34 -0500

    Actualizacion en el archivo de rutas para cargar archivos

--- server/api/routes/riseRoutes.js
+++ server/api/routes/riseRoutes.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const app = express();
-const fileRoute = require('../../uploads/');
 const fileUpload = require('express-fileupload');
-const router = express.Router();
 
-app.use(fileUpload());
-
-router.post('/upload', (req, res) => {
-  if (req.fields) {
-    var uploadFile = req.files.uploadFile;
-    var fileName = uploadFile.name;
-
-    uploadFile.mv('../../uploads/' + fileName, (err) => {
-      if (err) {
-        res.json({ 'response': res.status(500) });
-        res.send(err);
-        res.end('response');
-      } else {
-        res.send('File uploaded!');
-      }
-    });
-  }
-});
-
-module.exports = router;
+module.exports = (app) => {
+  app.use(fileUpload());
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
+  app.post('/upload', (req, res) => {
+    console.log('ruta de carga');
+    console.log(req.body);
+    if (req.fields) {
+      console.log('ruta de carga2');
+      var uploadFile = req.files.uploadFile;
+      var fileName = uploadFile.name;
+      
+      uploadFile.mv('./cargas/' + fileName, (err) => {
+        if (err) {
+          //res.json({ 'response': res.status(500) });
+          res.send(err);
+          res.end('response');
+        } else {
+          res.send('File uploaded!');
+        }
+      });
+    } else {
+      //res.send(res.status);
+      res.send(500);
+      res.end('Finaliza el servicio');
+    }
+  });
+};
